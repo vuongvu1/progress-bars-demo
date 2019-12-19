8b4b4e74aa8d43c2832193c5c88eadee7b3e0ea3
vuongvu <hoangvuong.v99@gmail.com>
2019-12-20 00:46:35 +0700

    [#3] Add eslint configs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import store, { history } from './store';
 import App from 'containers/App';
+import store, { history } from './store';
 import * as serviceWorker from './serviceWorker';
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
+      <App />
     </ConnectedRouter>
   </Provider>,
   document.querySelector('#root'),
